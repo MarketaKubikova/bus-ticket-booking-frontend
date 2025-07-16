f8533c6cb0b01c99cbfefccd2271a9a454a14d85
Markéta Kubíková <73794455+MarketaKubikova@users.noreply.github.com>
2025-07-16 21:55:31 +0200

    added openapi generator (#4)
    * added openapi generator
    
    * generated api sources

---
+++ src/apis/index.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthenticationApi';
+export * from './BusManagementApi';
+export * from './CityManagementApi';
+export * from './PaymentProcessingApi';
+export * from './ReservationManagementApi';
+export * from './RouteManagementApi';
+export * from './ScheduledTripManagementApi';
+export * from './SeatManagementApi';
+export * from './WalletManagementApi';
